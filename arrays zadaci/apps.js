6a05a3346a5f633d66f8b42a7c3867632126f269
ajlalomnicanin <ajlalomnicanin11@gmail.com>
2023-06-25 00:47:01 +0200

    arrays zadaci

--- arrays zadaci/apps.js
+++ arrays zadaci/apps.js
@@ -23,6 +23,72 @@ console.log(last([7, 9, 0, -2], 6));
 // Sample array : myColor = ["Red", "Green", "White", "Black"];
 // Expected Output :
 // "Red,Green,White,Black"
-// "Red,Green,White,Black"
+// "Red,Green,White,Black" // join
 // "Red+Green+White+Black"
 
+function myColor(arr) {
+  return arr.toString();
+}
+console.log(myColor(["Red", "Green", "White", "Black"]));
+//
+function myColorr(arr) {
+  return arr.join("+");
+}
+console.log(myColorr(["Red", "Green", "White", "Black"]));
+
+// 6.Write a JavaScript program that accepts a number as input and inserts dashes
+//  (-) between each even number. For example if you accept 025468 the output should be 0-254-6-8.
+
+function first(num) {
+  let noviBroj = num[0];
+  for (let i = 1; i < num.length; i++) {
+    if (num[i] % 2 === 0 && num[i - 1] % 2 === 0) {
+      noviBroj += "-" + num[i];
+    } else {
+      noviBroj += num[i];
+    }
+  }
+  return noviBroj;
+}
+console.log(first("025468"));
+
+// 7. Write a JavaScript program to sort the items of an array.
+// Sample array : var arr1 = [ -3, 8, 7, 6, 5, -4, 3, 2, 1 ];
+// Sample Output : -4,-3,1,2,3,5,6,7,8
+
+function sortniz(niz) {
+  return niz.sort(function (a, b) {
+    return a - b;
+  });
+}
+console.log(sortniz([-3, 8, 7, 6, 5, -4, 3, 2, 1]));
+
+// 8. Write a JavaScript program to find the most frequent item in an array.
+// Sample array : var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+// Sample Output : a ( 5 times )
+
+function aFind(arr) {
+  countur = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "a") {
+      countur++;
+    }
+  }
+  return countur;
+}
+console.log(aFind([3, "a", "a", "a", 2, 3, "a", 3, "a", 2, 4, 9, 3]));
+
+// 9. Write a JavaScript program that accepts a string as input and swaps the case of each character
+// . For example if you input 'The Quick Brown Fox' the output should be 'tHE qUICK bROWN fOX'.
+
+function casse(string) {
+  let newString = "";
+  for (let i = 0; i < string.length; i++)
+    if (string[i] === string[i].toUpperCase()) {
+      newString += string[i].toLowerCase();
+    } else {
+      newString += string[i].toUpperCase();
+    }
+  return newString;
+}
+console.log(casse("The Quick Brown Fox"));
