31f6e98e46300724dc2c2289cbd16eeeaf7fefea
ajlalomnicanin <ajlalomnicanin11@gmail.com>
2023-06-29 20:56:53 +0200

    zadaci arrays

--- arrays zadaci/apps2.js
+++ arrays zadaci/apps2.js
@@ -20,3 +20,63 @@ color = ["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "];
 o = ["th", "st", "nd", "rd"];
 
 console.log(first(color, o));
+
+// 6. Write a JavaScript program to find the leap years in a given range of years.
+function years(starYear, endYear) {
+  let prestupneG = [];
+  for (let year = starYear; year <= endYear; year++) {
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+      prestupneG.push(year);
+    }
+  }
+  return prestupneG;
+}
+console.log(years(2000, 2020));
+// Prestupna godina je godina koja ima 366 dana umesto standardnih 365 dana. Dodatni dan se dodaje 29.
+//  februara. Postoji nekoliko pravila za određivanje prestupnih godina:
+
+// Godine deljive sa 4 su prestupne godine. Na primer, 2012. godina je prestupna jer je deljiva sa 4.
+// Međutim, godine deljive sa 100 nisu uvek prestupne. Na primer, 1900. godina je deljiva sa 4, ali nije
+//  prestupna jer je takođe deljiva sa 100.
+// Izuzetak od drugog pravila je da godine deljive sa 400 ipak budu prestupne. Na primer, 2000. godina je
+//  prestupna jer je deljiva sa 400.
+
+// 17. Write a JavaScript program to shuffle an array.
+
+function shuffleArray(arr) {
+  return arr.sort(() => Math.random() - 0.5);
+}
+console.log(shuffleArray([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+// 18. Write a JavaScript program to perform a binary search.
+// Note : A binary search or half-interval search algorithm finds the position of a specified input value within an array sorted by key value.
+// Sample array :
+// var items = [1, 2, 3, 4, 5, 7, 8, 9];
+// Expected Output :
+// console.log(binary_Search(items, 1)); //0
+// console.log(binary_Search(items, 5)); //4
+
+function first(niz, broj) {
+  let pocetak = 0;
+  let kraj = niz.length - 1;
+  while (pocetak <= kraj) {
+    let sredina = Math.floor((pocetak + kraj) / 2);
+    if (niz[sredina] === broj) {
+      return sredina;
+    } else if (niz[sredina] < broj) {
+      pocetak = sredina - 1;
+    } else {
+      kraj = sredina + 1;
+    }
+  }
+  return ` Ne postoji taj broj`;
+}
+
+console.log(first(niz, 1));
+console.log(first(niz, 5));
+
+// 19. There are two arrays with individual values. Write a JavaScript program to compute the sum of each individual index value in the given array.
+// Sample array :
+// array1 = [1,0,2,3,4];
+// array2 = [3,5,6,7,8,13];
+// Expected Output :
+// [4, 5, 8, 10, 12, 13]
