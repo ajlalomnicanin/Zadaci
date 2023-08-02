09ccb4d6f27ed911beba3b3d4e7b066f1ecf1926
ajlalomnicanin <ajlalomnicanin11@gmail.com>
2023-08-02 23:55:55 +0200

    exersises

--- object-array zadaci/app1.js
+++ object-array zadaci/app1.js
@@ -2,6 +2,19 @@
 // Volume of a cylinder : V = πr2h
 // where r is the radius and h is the height of the cylinder.
 
+class Cilindar {
+  constructor(visina, poluprecnik) {
+    this.visina = visina;
+    this.poluprecnik = poluprecnik;
+  }
+  zapremina() {
+    return (Math.PI * this.poluprecnik ** 2 * this.visina).toFixed(4);
+  }
+}
+var cilinder = new Cilindar(5, 8); //
+console.log(cilinder.zapremina());
+
+
 console.log("6.zadatak");
 // Write a bubble sort algorithm in JavaScript.
 // Note : Bubble sort is a simple sorting algorithm that works by repeatedly stepping through the list to be sorted,
@@ -10,7 +23,49 @@ const arr = [6, 4, 0, 3, -2, 1];
 let arr2 = arr.sort((a, b) => a - b);
 console.log(arr2);
 
+
 console.log("7. zadatak");
 // 7. Write a JavaScript program that returns a subset of a string.
 // Sample Data: dog
 // Expected Output: ["d", "do", "dog", "o", "og", "g"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
